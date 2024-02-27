29f428d442909369afbecdd70e840568944f42c3
Patrick <patrick@thetrailerpartsoutlet.com>
2024-02-26 19:42:39 -0600

    2.26.24 - Check DEV_NOTES for details

--- assets/DEV-LOG.js
+++ assets/DEV-LOG.js
@@ -24,3 +24,30 @@
     - New B2B wholesale form implimentation
     - Linkable Headers
     - Various Requested Style Changes
+
+// 2.26.24
+/*
+Title Change on Variant Switch  - New Variant Selects method that changes title to public title on switch. 
+Product Info Style Updates: 
+Reduced margin and padding on most product info elements for better space and balancing. 
+Implemented color changes based on insights from A/B test. Variant buttons black/white, gray backdrop for section separation, and stark blue only for items that carry higher significance (reviews,and add to cart).
+Reduced title size
+Replaced image carousel with number indicator and arrow switches
+
+Sticky add to cart menu - New code watches page position of main add to cart button and activates a slide up sticky menu when main ATC is out of view. The slide up menu is fully dynamic including image, title, and price changes as the variant is changed. 
+Refactored “Call when available” feature to be more efficient and update to multiple locations. 
+Fixed forced scroll issue on Brake assembly pages.
+Optimized the newsletter backdrop with updated liquid code to better pull responsive images and allow for alt image text.
+Site wide speed optimizations (home page - emphasis on Core web vitals):
+Set key stylesheets to preload and deferred all scripts possible.
+Refactored hero section to pull and present responsive images server side.
+Set up hero image, banner, and truck icon to high priority preload; decreasing LCP and CLS as a result.
+Refactored fonts to pull from our hosting instead of external source. Had to gather up all font files, convert, then pull into our theme. 
+Applied lazy loading to all images below the fold. 
+Minimized major CSS and Script files
+Moved hero CSS to its own file to be preloaded. 
+Fixed newsletter backdrop and delivery truck icon to allow for alt image text
+Gave links aria-labels to improve accessibility score.
+Refactored multiple cart elements to not have the same id.
+Increased sizing on search bar and footer menu links to improve tab target coverage (SEO score)
+*/
